85c35455d992aec3923a883a4721991c74cca486
Sadziu <105722423+Sadziu01@users.noreply.github.com>
2023-03-14 23:18:48 +0100

    Add files via upload

---
+++ Generics/Generics/PaginatedResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+
+    class Restaurant : IEntity
+    {
+        public int Id { get; set; }
+    }
+
+    class User : IEntity
+    {
+        public User()
+        {
+
+        }
+
+        public User(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+
+    internal class PaginatedResult<T>
+    {
+        public List<T> Results { get; set; }
+        public int PageFrom { get; set; }
+        public int PageTo { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalResults { get; set; }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,40 @@
+﻿using Generics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var result = new PaginatedResult<Restaurant>();
+
+        var restauratns = new List<Restaurant>();
+        var users = new List<User>();
+
+        result.Results = restauratns;
+        //result.Results = users; //compile error
+
+
+        //var stringRepository = new Repository<string>();
+
+        //stringRepository.AddElement("some value");
+
+        //string firstElement = stringRepository.GetElement(0);
+
+
+        var user2Repository = new Repository<User>();
+
+
+        var userRepository = new Repository<string, User>();
+
+        userRepository.AddElement("Bill", new User() { Name = "Bill" });
+
+        var bill = userRepository.GetElement("Bill");
+
+
+
+        int[] intArray = new[] { 1, 3, 5 };
+
+        Utils.Swap(ref intArray[0], ref intArray[2]);
+
+        Console.WriteLine(string.Join(", ", intArray));
+    }
+}
